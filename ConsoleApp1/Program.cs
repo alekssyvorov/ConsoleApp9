2d0541094e9c7210faa1eec0403d40cddb1b47be
Evgeniy <alekssyvorov29@gmail.com>
2023-02-26 18:26:30 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "5";
+            string b = "1";
+
+            Add(a, b);
+
+            Console.ReadLine();
+        }
+        static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Add(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Add(string a, string b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
